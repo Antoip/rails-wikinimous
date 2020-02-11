30c0a0561d43aae07ccb82daf0fac652848df884
Antoine Piguet <antoine.piguet@gmail.com>
2020-02-11 18:20:26 +0100

    Added an add feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,11 +8,13 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
-
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
   end
 
   def edit
@@ -26,4 +28,11 @@ class ArticlesController < ApplicationController
   def destroy
 
   end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
 end
